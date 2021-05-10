01c067e1b83175cb3f03c2061528393166173012
Doug Davis <ddavis@ddavis.io>
2021-05-10 11:58:26 -0400

    0.3.0

--- numpydoc.el
+++ numpydoc.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Doug Davis <ddavis@ddavis.io>
 ;; URL: https://github.com/douglasdavis/numpydoc.el
 ;; SPDX-License-Identifier: GPL-3.0-or-later
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "25.1") (s "1.12.0") (dash "2.18.0"))
 ;; Keywords: convenience
 
